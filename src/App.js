129e0282f99e4d14d118349fbe1f7ee5ea0c8b82
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-05-06 16:37:35 +0200

    Graph 1 data calculated

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3';
+import _ from 'lodash';
+
+var surveyQs = {
+  percents: {
+    dataeng: 'Percent of your day focused on data engineering?',
+    dataprep: 'Percent of your day focused on data prep work?',
+    datasci: 'Percent of your day focused on data science?',
+    design: 'Percent of your day focused on design?',
+    dataviz: 'Percent of your day focused on creating/implementing/productizing data visualizations?',
+  },
+  employment: "What's your employment status",
+  vizmoreless: 'Do you want to spend more time or less time visualizing data in the future?',
+  vizfrustrations: 'What is your biggest frustration with doing data visualization in your job?',
+};
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {survey: []};
+  }
+
+  componentWillMount() {
+    d3.csv(process.env.PUBLIC_URL + '/data/survey.csv', survey => {
+      survey = _.filter(survey, d => d[surveyQs.employment] === 'Full-time');
+      this.setState({survey});
+    });
+  }
+
   render() {
+    // % of time ppl spend doing X, separated by if they want to do more/less viz
+    // first divided by more/less, then by the percents
+    var graph1Data = _.chain(this.state.survey)
+      // make sure they have answer
+      .filter(d => d[surveyQs.vizmoreless] && d[surveyQs.vizmoreless] !== 'Same')
+       // get either "less" or "more"
+      .groupBy(d => _.last(d[surveyQs.vizmoreless].split(' ')).toLowerCase())
+      .map((answers, type) => {
+        return {
+          type,
+          bars: _.map(surveyQs.percents, type => {
+            return {
+              type,
+              bars: _.filter(answers, a => a[type]),
+            }
+          }),
+        }
+      }).value();
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
