d9e141bd8d5501762e9923b03d5c9506aa84b4d8
Shirley Wu <shirleyxueyangwu@gmail.com>
2017-07-17 16:56:43 -0700

    Intro paragraphs

--- src/Intro.js
+++ src/Intro.js
@@ -44,6 +44,7 @@ class Intro extends Component {
       width: 1000,
       margin: '60px auto',
       textAlign: 'center',
+      lineHeight: 2,
     };
 
     return (
@@ -55,6 +56,14 @@ class Intro extends Component {
         <sup>
           BY <a href='http://twitter.com/sxywu' target='_new'>SHIRLEY WU</a>
         </sup>
+
+        <p>
+Earlier this year, my friend <a href='http://twitter.com/Elijah_Meeks'>Elijah</a> made a bold claim: that most people in data visualization end up leaving the field, because there's something wrong with the current state of data visualization.  It stirred quite a bit of conversation, and resulted in a <a href='https://medium.com/@Elijah_Meeks/2017-data-visualization-survey-results-40688830b9f2' target='_new'>community survey</a> and a <a href='https://medium.com/visualizing-the-field/visualizing-the-field-finding-our-way-6330d3e48555' target='_new'>Medium publication</a>.  The survey itself had <strong>45 questions</strong>, and ranged from asking for demographic information to the role of data visualization in the respondent's job.  It garnered <strong>981 responses</strong>, and out of curiosity, I decided to dig into their answers.
+        </p>
+
+        <p>
+Since none of the <a href='https://github.com/emeeks/data_visualization_survey/blob/master/data/questions.txt' target='_new'>survey questions</a> could actually measure whether people are leaving the field, I focused instead on their frustrations.  I wanted to know whether more or less people had frustrations, and how that number correlated with other aspects of their data visualization jobs: if they were hired to do data visualization, were they more likely to have frustrations?  What about if they were paid more or less than their UI and design counterparts, or if they worked collaboratively or consultatively with their consumer?  By looking at the frustrations that come from specific parts of their jobs, I'm hoping to identify the areas that we as a community can work to better.
+        </p>
       </div>
     );
   }
